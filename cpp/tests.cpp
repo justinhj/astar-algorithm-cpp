22fd6ce71a9e177d9108c9f1bf1eadb6a6edb1a2
Justin Heyes-Jones <justinhj.iggca@gmail.com>
2017-01-12 11:57:43 -0800

    add simple test runner

---
+++ cpp/tests.cpp
@@ -0,0 +1,13 @@
+// A unit test suite
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(int argc, char *argv[]) {
+
+	assert(true && "failed to be true");
+
+	printf("Tests succeeded\n");
+
+}
