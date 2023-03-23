b9062bdd88cf8d256a9e85bb724904d57e741679
Brian Cochran <brian@greenzie.com>
2023-03-23 18:33:06 -0400

    remove unnecessary namespace

--- cpp/stlastar.h
+++ cpp/stlastar.h
@@ -831,7 +831,7 @@ public:
 	virtual bool GetSuccessors( AStarSearch<T> *astarsearch, T *parent_node ) = 0; // Retrieves all successors to this node and adds them via astarsearch.addSuccessor()
 	virtual float GetCost( T &successor ) = 0; // Computes the cost of travelling from this node to the successor node
 	virtual bool IsSameState( T &rhs ) = 0; // Returns true if this node is the same as the rhs node
-	virtual std::size_t Hash() = 0; // Returns a hash for the state
+	virtual size_t Hash() = 0; // Returns a hash for the state
 };
 
 #endif
